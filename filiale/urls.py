aa011265f8ac2395b4b071a3f46c759f42fb98f4
simo- <simo-arena@hotmail.it>
2025-10-16 15:43:06 +0200

    profoundly modified the website. The website is static and not dynamic anymore.

--- filiale/urls.py
+++ filiale/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 # app_name è fondamentale per l'uso dei tag {% url 'nome_rotta' %}
 app_name = 'filiale'
@@ -10,8 +11,8 @@ urlpatterns = [
     path('servizi/', views.servizi, name='servizi'),
     path('servizi/<int:pk>/', views.dettaglio_servizio, name='dettaglio_servizio'),
     path('chi-siamo/', views.chi_siamo, name='chi_siamo'),
-    path('contatti/', views.contatti, name='contatti'),
     path('documentazione/', views.documentazione, name='documentazione'),
-    path('richiedi-preventivo/', views.richiedi_preventivo, name='richiedi_preventivo'),
-    path('prenota-appuntamento/', views.prenota_appuntamento, name='prenota_appuntamento'),
+    path('richiedi-preventivo/', TemplateView.as_view(template_name='filiale/richiedi_preventivo.html'), name='richiedi_preventivo'),
+    path('prenota-appuntamento/', TemplateView.as_view(template_name='filiale/prenota_appuntamento.html'), name='prenota_appuntamento'),
+    path('contatti/', TemplateView.as_view(template_name='filiale/dove_siamo.html'), name='contatti'),
 ]
--- santander_filiale/settings.py
+++ santander_filiale/settings.py
@@ -11,10 +11,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', None)
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-*5^khahyts_p-gxyqg+vr#jui6xio8*dvh-b_gfhy+fi3m$t(_')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = [
@@ -51,12 +51,12 @@ MIDDLEWARE = [
 ]
 
 # Impostazioni di sicurezza per forzare HTTPS
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = False
 SECURE_HSTS_SECONDS = 3600
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
 
 ROOT_URLCONF = 'santander_filiale.urls'
 
@@ -84,12 +84,12 @@ WSGI_APPLICATION = 'santander_filiale.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'USER': os.environ.get('DB_USER'),
+        # 'PASSWORD': os.environ.get('DB_PASSWORD'),
+        # 'HOST': os.environ.get('DB_HOST'),
+        # 'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
